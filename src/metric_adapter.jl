045c3eedb47df6dbd40a196f1a308ee4044e9ee8
Edward A. Roualdes <eroualdes@csuchico.edu>
2023-01-28 14:46:36 -0800

    add Fisher divergence tuned metric; what do I test this against? it seems to be biased small

--- src/metric_adapter.jl
+++ src/metric_adapter.jl
@@ -64,3 +64,35 @@ end
 function set!(sampler, meca::MetricECA, idx; kwargs...)
     sampler.metric[:, idx] .= meca.metric[:, idx]
 end
+
+struct MetricFisherDivergence{T<:AbstractFloat} <: AbstractMetricAdapter{T}
+    om::OnlineMoments{T}
+    og::OnlineMoments{T}
+    metric::Matrix{T}
+end
+
+function MetricFisherDivergence(initial_metric::AbstractMatrix{T}; kwargs...) where {T}
+    dims, metrics = size(initial_metric)
+    om = OnlineMoments(T, dims, metrics)
+    og = OnlineMoments(T, dims, metrics)
+    return MetricFisherDivergence(om, og, initial_metric)
+end
+
+function update!(mfd::MetricFisherDivergence, x::AbstractMatrix, g::AbstractMatrix; kwargs...)
+    update!(mfd.om, x; kwargs...)
+    update!(mfd.og, g; kwargs...)
+end
+
+function optimum(mfd::MetricFisherDivergence; kwargs...)
+    T = eltype(mfd.om.v)
+    if mfd.om.n[1] > 1
+        return sqrt.(mfd.om.v ./ mfd.og.v)
+    else
+        return ones(T, size(mfd.om.v))
+    end
+end
+
+function reset!(mfd::MetricFisherDivergence; kwargs...)
+    reset!(mfd.om; kwargs...)
+    reset!(mfd.og; kwargs...)
+end
